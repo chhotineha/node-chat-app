cc2ec0d372b93fd1111909143143549f917e1215
Neha Kumari <M1048061@mindtree.com>
2018-11-19 17:07:32 +0530

    new chatbot app

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+const app = express();
+app.use(express.static(publicPath));
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log("Hello you started new application")
+})
+module.exports = {
+    app: app
+}
